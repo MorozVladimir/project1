e4912d98360f1dbbdd59b153e92ae2d6f1dd4e41
Vladimir Moroz <morozvvlad@gmail.com>
2018-10-27 13:31:57 +0300

    second modefire

--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 		session_start();
 		echo("server test");
 		echo("<h2>Template ok</h2>");
+		include('tpl/base.php');
---
+++ tpl/base.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<titl>Базовый шаблон</title>
+</head>
+<body>
+	Базовый шаблон
+</body>
+</html>
